a29bc988beb30bcc195e41bfb4625b0372f19b63
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2022-03-16 20:29:28 +0530

    Fixes

--- dataloader.py
+++ dataloader.py
@@ -5,54 +5,43 @@ from PIL import Image
 from os import listdir
 from os.path import isfile, join
 from torch.utils.data import DataLoader, Dataset
-import os
+from pathlib import Path
 import random
 
-path = os.path.join(os.getcwd(),'dataset','mpii_human_pose_v1', 'images' )
+path = Path(__file__).parents[0] / "dataset" / "mpii_human_pose_v1" / "images"
 files = sorted([join(path, f) for f in listdir(path) if isfile(join(path, f))])
 
 random.seed(69)
 random.shuffle(files)
 
 split_ratio = 0.7
-train_size = int(split_ratio*len(files))
+train_size = int(split_ratio * len(files))
 test_size = len(files) - train_size
 
 transform = transforms.ToTensor()
 
-train_imgs, test_imgs = torch.zeros(train_size, 3, 256, 256), torch.zeros(test_size, 3, 256, 256)
-
-for i, f in enumerate(files):
-    img = Image.open(f).resize((256,256))
-
-    if i<train_size:
-        train_imgs[i, :, :] = transform(img).view((3, 256, 256))
-    else:
-        test_imgs[i, :, :] = transform(img).view((3, 256, 256))
-
-
+train_imgs, test_imgs = files[:train_size], files[train_size:]
 
 
 class MPI(Dataset):
     def __init__(self, images, device):
         self.imgs = images
-
-        self.imgs = self.imgs.to(device)
+        self.device = device
 
     def __len__(self):
         return len(self.imgs)
 
     def __getitem__(self, idx):
-        return self.imgs[idx, :, :]
+        img = Image.open(self.imgs[idx])
+        return transform(img.resize((128, 128))).view((3, 128, 128)).to(
+            self.device
+        ), transform(img.resize((256, 256))).view((3, 256, 256)).to(self.device)
 
-device = 'cpu'
+
+device = "cpu"
 
 trainset = MPI(train_imgs, device)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=False)          
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=False)
 
 valset = MPI(test_imgs, device)
 valloader = torch.utils.data.DataLoader(valset, batch_size=32, shuffle=False)
-    
-
-
-
--- train.py
+++ train.py
@@ -3,52 +3,58 @@ import torch.nn as nn
 from dataloader import trainloader, valloader
 from swin_ir import SwinIR
 from utils.psnr import calculate_psnr
+from time import time
 
-input_height, input_width = 256, 256 #Actual dimensions of image from DataLoader
-upscale = 4
+input_height, input_width = 128, 128  # Actual dimensions of image from DataLoader
+upscale = 2
 window_size = 8
 
-#Height and Width of each patch
+# Height and Width of each patch
 height = (input_height // upscale // window_size + 1) * window_size
 width = (input_width // upscale // window_size + 1) * window_size
 
 
-model =     model = SwinIR(
-        upscale=2,
-        img_size=(height, width),
-        window_size=window_size,
-        img_range=1.0,
-        depths=[6, 6, 6, 6],
-        embed_dim=60,
-        num_heads=[6, 6, 6, 6],
-        mlp_ratio=2,
-        upsampler="pixelshuffledirect",
-    )
+model = SwinIR(
+    upscale=upscale,
+    img_size=(input_height, input_width),
+    patch_size=(height, width),
+    window_size=window_size,
+    img_range=1.0,
+    depths=[4, 4,4, 4],
+    embed_dim=60,
+    num_heads=[4,4,4,4],
+    mlp_ratio=2,
+    upsampler="pixelshuffledirect",
+)
 
 optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
 
 epochs = 10
 
 for i in range(epochs):
-    
+
     for seq in trainloader:
-        labels = seq
+        stt = time()
+        x, y = seq
 
         optimizer.zero_grad()
-        y_pred = model(seq)
-        single_loss = calculate_psnr(y_pred, labels)
-        
+        y_pred = model(x)
+        single_loss = calculate_psnr(y_pred, y)
+
         single_loss.backward()
         optimizer.step()
 
         train_loss = single_loss.item()
-    
-    for seq in valloader:
-        labels = seq
-        y_pred - model(seq)
-        single_loss = calculate_psnr(y_pred, labels)
+        print(stt - time())
 
-        val_loss = single_loss.item()
+    with torch.no_grad():
+        for seq in valloader:
+            x, y = seq
+            y_pred = model(x)
+            single_loss = calculate_psnr(y_pred, y)
 
+            val_loss = single_loss.item()
 
-    print(f'epoch: {i:3} training loss: {train_loss:10.8f} ,  validation loss: {val_loss:10.8f} ')
+    print(
+        f"epoch: {i:3} training loss: {train_loss:10.8f} ,  validation loss: {val_loss:10.8f} "
+    )

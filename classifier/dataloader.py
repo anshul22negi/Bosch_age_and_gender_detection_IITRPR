991915c907ce76babdbe6573d9cb51c765d3f101
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2022-03-19 17:03:18 +0530

    stuff

--- classifier/dataloader.py
+++ classifier/dataloader.py
@@ -1,56 +1,62 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np  # linear algebra
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 import os
-from os.path import dirname, abspath
-
+from os.path import dirname, abspath, isfile
 
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from PIL import Image
 
 
 from torch.utils.data import Dataset, DataLoader
 import torchvision
 from torchvision import transforms
 
-
-from sklearn.model_selection import train_test_split
-
+import random
 
 
 BATCH_SIZE = 32
-data_file = os.path.join(dirname(dirname(abspath(__file__))), 'dataset', 'UTK', 'age_gender.csv')
+NBINS = 10
+BSETS = 10
+base = os.path.join(dirname(dirname(abspath(__file__))), "dataset", "utkface")
+files = [f for f in os.listdir(base) if isfile(join(base, f))]
 
-#Reading the csv file 
-df = pd.read_csv('../input/age-gender-and-ethnicity-face-data-csv/age_gender.csv')
+random.seed(69)
+split = 0.7
+train_len = int(split * len(files))
+trainfiles = files[:train_len]
+valfiles = files[train_len:]
+
+bins = [[0] + sorted(random.sample(range(117), NBINS - 1)) for _ in range(NSETS)]
 
 
 class get_data(Dataset):
-    def __init__(self, df):
-        self.df = df
-        self.transform = transforms.Compose([
-            transforms.ToTensor(),
-            
-        ])
-        
+    def __init__(self, f):
+        self.files = [join(base, x) for x in f]
+        self.ages = [int(x.split("_")[0]) for x in f]
+        for i in range(len(f)):
+            vals = torch.zeros(NBINs, dtype=torch.float32)
+            for j in range(NSETS):
+                for k in range(NBINS):
+                    if self.ages[i] >= bins[j][k]:
+                        vals[j] = k
+            self.ages[i] = vals
+
+        self.genders = [
+            F.one_hot(torch.tensor(int(x.split("_")[1])), num_classes=2) for x in f
+        ]
+
     def __len__(self):
-        return len(self.df)
-    
-    def __getitem__(self,i):
-        age = df['age'][i]
-        gender = df['gender'][i]
-        
-        im = df['pixels'][i]
-        im = np.reshape(im, (256,256, 3))
-        im = self.transform(im)/255.0        
-        age = torch.tensor(age)
-        gender = torch.tensor(gender)
+        return len(self.f)
+
+    def __getitem__(self, i):
+        img = Image.open(self.files[i])
         
         return im, age, gender
-    
 
-train, test = train_test_split(df, test_size=0.3, random_state=69) 
+
+train, test = train_test_split(df, test_size=0.3, random_state=69)
 train_loader = DataLoader(get_data(train), batch_size=BATCH_SIZE, shuffle=True)
 test_loader = DataLoader(get_data(test), batch_size=BATCH_SIZE, shuffle=False)
-
--- classifier/model.py
+++ classifier/model.py
@@ -4,45 +4,48 @@ import torch.nn as nn
 
 
 class classifier_model(nn.Module):
-    def __init__(self, output_buckets):
+    def __init__(self, output_buckets, num_bucket_sets):
         super(classifier_model, self).__init__()
-        self.model_ft = models.vgg16(pretrained = True)
+        self.model_ft = models.vgg16(pretrained=True)
+        self.n_buckets = output_buckets
+        self.n_sets = num_bucket_sets
 
-        
-        self.age_classifier_layer = nn.Sequential(
-            nn.Linear(1000, 256, bias = True), 
-             nn.BatchNorm1d(256),
-             nn.Dropout(0.2),
-             nn.Linear(256 , 128, bias = True),
-             nn.Linear(128 , output_buckets)
-             
+        self.pre_age_classifier_layer = nn.Sequential(
+            nn.Linear(1000, 256, bias=True),
+            nn.BatchNorm1d(256),
+            nn.Dropout(0.2),
+            nn.Linear(256, 128, bias=True),
         )
 
+        self.bin_layers = [
+            nn.Sequential(nn.Linear(128, output_buckets, bias=False), nn.LogSoftmax(dim=1))
+            for _ in range(num_bucket_sets)
+        ]
+
         self.gender_classifier_layer = nn.Sequential(
-            nn.Linear(1000, 256, bias = True), 
-             nn.BatchNorm1d(256),
-             nn.Dropout(0.3),
-             nn.Linear(256 , 128, bias = True),
-             nn.Linear(128 , output_buckets)
-             
-        )   
-        
+            nn.Linear(1000, 256, bias=True),
+            nn.BatchNorm1d(256),
+            nn.Dropout(0.3),
+            nn.Linear(256, 128, bias=True),
+            nn.Linear(128, 2),
+            nn.Softmax(dim=1)
+        )
+
     def forward(self, x):
         x = self.model_ft(x)
+
+        age = self.pre_age_classifier_layer(x)
+        y = torch.zeros((x.shape[0], self.n_sets, self.n_buckets), dtype=x.dtype)
+        for i in range(self.n_sets):
+            y[:, i, :] = self.bin_layers[i](age)
         
-        age = self.age_classifier_layer(x)
         gender = self.gender_classifier_layer(x)
-        
-        return age, gender
-    
-    
-if __name__ == '__main__':
+
+        return y, gender
+
+
+if __name__ == "__main__":
     model = classifier_model(5)
-    x = torch.randn((32,3, 256, 256))
-    x,y = model(x)
+    x = torch.randn((32, 3, 256, 256))
+    x, y = model(x)
     print(x.shape)
-        
-        
-    
-
-

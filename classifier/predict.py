fe44468389be5231967f7fc063daa6462daad9ad
anshul22negi <anshul22negi@gmail.com>
2022-03-21 12:41:53 +0530

    classifier predict update

--- classifier/predict.py
+++ classifier/predict.py
@@ -25,14 +25,31 @@ def predict_gender_and_age(image_path):
 
     with torch.no_grad():
         pred_ages, pred_gender = model()
+        age_low,age_high = 0, 116
+        max_confidence = 0
+        pred_ages = torch.squeeze(pred_ages,0)
         for i in range(pred_ages.size()[0]):
             for j in range(pred_ages.size()[1]):
                 if j < pred_ages.size()[1]-1:
                     age_prediction += (bins[i][j] + bins[i+1][j])*pred_ages[i][j]/2
                 else:
                     age_prediction += (bins[i][j] + 116)*pred_ages[i][j]/2
+        
+
+        for i in range(pred_ages.size()[0]):
+            for j in range(pred_ages.size()[1]):
+                if j < pred_ages.size()[1]-1:
+                    if age_prediction >= bins[i][j] and age_prediction <= bins[i+1][j] and max_confidence<pred_ages[i][j]:
+                        max_confidence, age_low, age_high = pred_ages[i][j], bins[i][j], bins[i+1][j]
+                else:
+                    if age_prediction >= bins[i][j] and age_prediction <= 116 and max_confidence<pred_ages[i][j]:
+                        max_confidence, age_low, age_high = pred_ages[i][j], bins[i][j], 116
+
+                        
+
+
                     
-    return pred_gender,age_prediction
+    return pred_gender,age_prediction, age_low, age_high
 
 
 if __name__ == '__main__':
--- run_video.py
+++ run_video.py
@@ -3,6 +3,7 @@ import cv2
 import os
 from os.path import abspath, dirname
 import numpy as np
+import csv
 
 video_path = os.path.join(dirname(abspath(__file__)),'dataset', 'videoplayback.mp4')
 
@@ -13,15 +14,22 @@ cap = cv2.VideoCapture(video_path)
 if (cap.isOpened() == False):
     print("Error opening file")
 
+output_csv_file = ""
+csv_file = open(output_csv_file, 'wb')
+writer = csv.writer(csv_file)
+
+header_row = ['frame num', 'person id', 'bb_xmin', 'bb_ymin', 'bb_height', 'bb_width', 'age_min', 'age_max', 'age_actual', 'gender']
+writer.writerow(header_row)
+
+frame_number = 0
 while(cap.isOpened()):
     ret, frame = cap.read()
-
+    frame_number+=1
     if ret:
-        print(ret)
-        print(type(frame))
+        img = torch.Tensor(frame)
+
         cv2.imshow('Frame',frame)
-        if cv2.waitKey(200) & 0xFF == ord('q'):
-            break
+        
     else:
         break
 
